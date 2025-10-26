50743460b353dfcaf73ee7982fe6d292a8c5e635
Rachel Bronzen <162027353+rachelbronzen@users.noreply.github.com>
2025-10-26 23:32:52 +0700

    Delete tugas2des directory

--- tugas2des/buatkey.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Random import get_random_bytes
-import os
-
-#panjang 8 byte (64 bit)
-key = get_random_bytes(8)
-filename = "shared_key.key"
-with open(filename, "wb") as f:
-    f.write(key)
